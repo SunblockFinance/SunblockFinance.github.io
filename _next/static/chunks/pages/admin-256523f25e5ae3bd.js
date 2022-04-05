2a19544d8e3b2fdc91f08d66875e9514e038dfe8
Kenth Fagerlund <kenth@divit.com.hk>
2022-04-05 20:45:22 +0800

    initial commit

---
+++ _next/static/chunks/pages/admin-256523f25e5ae3bd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[964],{2347:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin",function(){return t(4544)}])},4544:function(n,u,t){"use strict";t.r(u);var r=t(7320);u.default=function(){return(0,r.tZ)("h1",{children:"Hello"})}}},function(n){n.O(0,[888,179],(function(){return u=2347,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
